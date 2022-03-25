80c5f37d10b92a529d75d432f5cb149e8e8ce58e
samuser89 <sdsv989@gmail.com>
2022-03-24 20:35:02 -0500

    Clase

---
+++ University.Web/Controllers/CoursesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace University.Web.Controllers
+{
+    public class CoursesController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
