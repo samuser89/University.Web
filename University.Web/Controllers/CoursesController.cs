745442f9574ead3d89cf204241573d3ae91aec03
samuser89 <sdsv989@gmail.com>
2022-03-24 21:32:02 -0500

    avance

--- University.Web/Controllers/CoursesController.cs
+++ University.Web/Controllers/CoursesController.cs
@@ -1,17 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Threading.Tasks;
 using System.Web.Mvc;
+using University.BL.Models;
+using University.BL.Repositories;
+using University.BL.Repositories.Implements;
 
 namespace University.Web.Controllers
 {
     public class CoursesController : Controller
     {
+        private static readonly ICourseRepository courseRepository = new CourseRepository(new UniversityModel());
         [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
+
+
+        [HttpGet]
+        public async Task<ActionResult> IndexJson()
+        {
+            var courses = await courseRepository.GetAll();
+            return Json(courses, JsonRequestBehavior.AllowGet);
+        }
     }
+
 }
